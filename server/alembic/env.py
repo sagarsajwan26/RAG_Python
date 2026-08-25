ee053d5fa3718711d47de8206784bb68f48597da
sagarsajwna26 <sagarsajwna26@gmail.com>
2026-08-25 16:36:13 +0530

    Add conversation and message alembic migrations

--- server/alembic/env.py
+++ server/alembic/env.py
@@ -4,7 +4,16 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy import pool
 from app.models import Base
 from alembic import context
-from app.models import TenantMember, User, Tenant, Document, RefreshToken, Chunk
+from app.models import (
+    TenantMember,
+    User,
+    Tenant,
+    Document,
+    RefreshToken,
+    Chunk,
+    Conversation,
+    Message,
+)
 
 from app.core.config import settings
 
