9664212ab9c39b1de96bbd6a679706a38c53c150
sagarsajwna26 <sagarsajwna26@gmail.com>
2026-08-26 16:27:24 +0530

    feat: use Cookie-based authentication instead of HTTP Bearer

--- server/app/core/dependencies.py
+++ server/app/core/dependencies.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Cookie
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.security import verify_access_token
@@ -11,22 +11,25 @@ bearer_scheme = HTTPBearer()
 
 
 async def get_current_user(
-    credentials: str = Depends(bearer_scheme), db: AsyncSession = Depends(get_db)
+    access_token: str | None = Cookie(default=None), db: AsyncSession = Depends(get_db)
 ) -> User:
-    token = credentials.credentials
-    user_id = verify_access_token(token)
+    if access_token is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="not authenticated"
+        )
+
+    user_id = verify_access_token(access_token)
     if user_id is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="invalid or expired access token",
-            headers={"www-Authenticate": "Bearer"},
+            detail="Invalid or expired access token",
         )
     repository = UserRepository(db)
     user = await repository.get_by_id(user_id)
+
     if user is None:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User not found",
-            headers={"WWW-Authenticate": "Bearer"},
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found"
         )
+
     return user
